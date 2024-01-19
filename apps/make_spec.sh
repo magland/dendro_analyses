da2fbcd7891383648f69f6358a137a9b6fe568e0
Jeremy Magland <magland@system76-pc.localdomain>
2024-01-19 07:07:06 -0500

    update spec

--- apps/make_spec.sh
+++ apps/make_spec.sh
@@ -2,5 +2,6 @@
 
 set -ex
 
-# spike_sorting_analysis
 dendro make-app-spec-file --app-dir dendro1 --spec-output-file dendro1/spec.json
+
+dendro make-app-spec-file --app-dir cebra1 --spec-output-file cebra1/spec.json
